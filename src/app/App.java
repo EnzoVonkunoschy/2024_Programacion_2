8491a0f941e4ef1a25edcbf226a2c40f4fa1c3a9
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2024-06-25 20:54:27 -0300

    Con suscriptores

--- src/app/App.java
+++ src/app/App.java
@@ -10,16 +10,24 @@ public class App {
         Destinatario des = new Destinatario("Ayelén");
         Mensaje men = new Mensaje(rem, des, "Me prestas unos pesos?");
 
-        Buzon.recibirMensaje(men);
+        Buzon unBuzon = new Buzon();
+        unBuzon.setNombre("Correo Argentino");
+
+        unBuzon.agregarSuscriptor(des);
+        
+
+        unBuzon.recibirMensaje(men);
 
         Destinatario des2 = new Destinatario("Enzo");
         Remitente rem2 = new Remitente("Vicente");
         Mensaje men2 = new Mensaje(rem2, des2, "¿Hay consulta?");
 
-        Buzon.recibirMensaje(men2);
+        unBuzon.agregarSuscriptor(des2);
 
-        Buzon.entregarMensaje(des);
-        Buzon.entregarMensaje(des2);
+        unBuzon.recibirMensaje(men2);
+
+        unBuzon.entregarMensaje(des);
+        unBuzon.entregarMensaje(des2);
 
     }
 }
